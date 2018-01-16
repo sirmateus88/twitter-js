31bd79fee004c8a4ba8f644b0905e6c26a43afca
Matthew Appleby <mappleby1@me.com>
2018-01-16 12:49:46 -0500

    made some routing

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const nunjucks = require( 'nunjucks' );
 const app = express(); // creates an instance of an express application
 const routes = require('./routes');
 app.use('/', routes);
+app.use(express.static('public'));
 
 const locals = {
   title: 'An Example',
@@ -14,9 +15,6 @@ const locals = {
 };
 
 nunjucks.configure('views', {noCache: true});
-// nunjucks.render('index.html', locals, function (err, output) {
-//   console.log(output);
-// });
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
@@ -28,7 +26,7 @@ app.use(function(req, res, next){
   next();
 });
 
-
+app.get('/*', (req, res) => (res.sendFile(__dirname + req.url)));
 
 app.listen(3000, () => console.log('server listening'));
 
