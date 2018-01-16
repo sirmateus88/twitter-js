d888432a148de9683ea21b543fcb6c1bd3bda51d
Matthew Appleby <mappleby1@me.com>
2018-01-16 15:08:53 -0500

    added linking to tweets and users

--- tweetBank.js
+++ tweetBank.js
@@ -2,7 +2,7 @@ const _ = require('lodash');
 const data = [];
 
 function add (name, content) {
-  data.push({ name: name, content: content });
+  data.push({ name: name, content: content, id: data.length});
 }
 
 function list () {
